eec7fd3c6d3369560f6829d58b3ef3dd31a80ad8
Ste-Vieira <stephanie_holzschuk@hotmail.com>
2022-06-22 21:31:48 -0300

    Scripts aula01

---
+++ aula01/Scripts.sql
@@ -0,0 +1,30 @@
+--DDL para criar uma tabela
+CREATE TABLE pessoa (
+    id   INTEGER PRIMARY KEY AUTOINCREMENT
+                 UNIQUE,
+    name TEXT    NOT NULL,
+    age  INTEGER
+);
+
+--DLL define objetos do banco de dados como tabelas e colunas
+create table usuario(
+id_usuario INTEGER UNIQUE PRIMARY KEY AUTOINCREMENT,
+matricula TEXT NOT NULL UNIQUE,
+nome TEXT NOT NULL
+);
+
+
+--DML manipula os dados do banco como inserts, update, delete e select
+--verificando se a tabela existe
+select * from pessoa; -- READ
+
+
+
+--inserindo um registro na tabela pessoa
+--CREATE
+--para inserir é necessario informar os campos e respectivamente os valores
+insert into pessoa (name,age) values("Rubem",32);
+
+
+--DELETE
+delete from pessoa;
